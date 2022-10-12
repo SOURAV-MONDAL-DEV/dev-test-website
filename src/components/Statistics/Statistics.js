626c01179134203484ee37f859590cb4b7996575
SOURAVMONDAL-BD <souravmondalcode@gmail.com>
2022-10-12 20:29:31 +0600

    delete all console.log

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -6,7 +6,6 @@ import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Resp
 
 const Statistics = () => {
     const topics = useLoaderData().data;
-    console.log(topics)
     return (
         <div className='chart-cont'>
             <BarChart width={400} height={400} data={topics}>
